1877464bc4023851eda37d0070f4e904d3a63a41
Morre <morre@mor.re>
2023-07-01 18:57:42 +0200

    fixup! chore(deps): update dependency @types/papaparse to v5.3.7

--- packages/ynap-parsers/src/util/papaparse.ts
+++ packages/ynap-parsers/src/util/papaparse.ts
@@ -3,7 +3,7 @@ import { parse as papaParse, ParseConfig, ParseResult } from 'papaparse';
 export const parse = (
   file: File | string,
   config?: ParseConfig,
-): Promise<ParseResult> =>
+): Promise<ParseResult<any>> =>
   new Promise((complete, error) => {
     papaParse(file as any, {
       ...config,
